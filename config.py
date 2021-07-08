0aee767452697e18a39a2a62d6efec8a38cc874d
TTXXR <250699925@qq.com>
2021-07-08 15:51:36 +0800

    21/7/8 add path

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 conf = {
-    "save_path": "C:/Users/rr/Desktop/sftp/mlp/trained-local",
-    "load_path": "C:/Users/rr/Desktop/sftp/mlp/trained-local",
+    # "save_path": "C:/Users/rr/Desktop/sftp/mlp/trained-local",  # win
+    # "load_path": "C:/Users/rr/Desktop/sftp/mlp/trained-local",
+    "save_path": "/home/rr/Downloads/Export/trained/mlp",  # linux
+    "load_path": "/home/rr/Downloads/Export/trained/mlp",
     "CUDA_VISIBLE_DEVICES": "0,1,2,3",
     "CUDA_USE": [0, 1, 2, 3],
-    "data": r"C:\Users\rr\Desktop\documents\Export",
+    # "data": r"C:\Users\rr\Desktop\documents\Export",  # win
+    "data": "/home/rr/Downloads/Export",  # linux
     "model": {
         'model_name': 'MLP',
         'epoch': 150,
